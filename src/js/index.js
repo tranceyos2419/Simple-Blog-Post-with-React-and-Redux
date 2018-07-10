1949cfc59d968d3027f96b316f0b6892a45928b9
tranceyos2419 <tranceyos2419@gmail.com>
2018-07-10 13:40:27 +0900

    test not working

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,7 @@
 import "../../views/index.html";
 import React from "react";
 import ReactDOM from "react-dom";
-
+import { BrowserRouter, Route, Link } from "react-router-dom";
 import { Provider } from "react-redux";
 import { createStore, applyMiddleware } from "redux";
 // import '../index.html'
@@ -9,10 +9,27 @@ import { createStore, applyMiddleware } from "redux";
 import App from "./components/app";
 import reducers from "./reducers/index";
 
+class Hello extends React.Component {
+  render() {
+    return <div>Hello</div>;
+  }
+}
+
+class Goodbye extends React.Component {
+  render() {
+    return <div>Goodbye</div>;
+  }
+}
+
 const createStoreWithMiddleware = applyMiddleware()(createStore);
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
+    <BrowserRouter>
+      <div>
+        <Route path="/hello" component={Hello} />
+        <Route path="/goodbye" component={Goodbye} />
+      </div>
+    </BrowserRouter>
   </Provider>,
   document.querySelector(".container")
 );
